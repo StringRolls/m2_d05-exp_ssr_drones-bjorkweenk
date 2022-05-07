3593dee47a73429a356aba9d70e2fdc17268b3ad
Bjork Weenk <bjorkweenk.bw@gmail.com>
2022-05-07 18:01:29 +0200

    iteration 0.5

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,18 @@
-// Iteration #1
+
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const droneSchema = new Schema(
+  {
+    // unless you are defining more than the "type" property, you don't have to use {} (see below)
+    // firstName: {type: String, require: true}
+    name: String,
+    propellers: Number,
+    maxSpeed: Number,
+  },
+);
+
+// const Book = model('Book', bookSchema);
+// module.exports = Book;
+
+module.exports = model("Drone", droneSchema);
